1526538bca64d8ba3e62d6f916541736c22d49e5
Pedro Martin <pataruco@gmail.com>
2018-08-01 09:52:02 +0100

    Set another scaffold

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,33 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: '[name]-[contenthash:8].css' })
+  ]
+};
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const h1 = () => {
+  const h1 = document.createElement('h1');
+  h1.innerText = 'Pedro';
+  return h1;
+};
+
+document.body.appendChild(h1());
