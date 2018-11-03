59f27ba6637ddf9d39bd7dda339bd6fab09c3ca0
Pedro Martin <pataruco@gmail.com>
2018-11-03 19:41:30 +0000

    Create intro-to-code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import getSlide from './lib/fetch-slides';
 // Using export-loader to load remark downloaded from NPM
 import remark from 'exports-loader?remark!remark-slide/out/remark.js';
 
-const lessons = {
+const fewdLessons = {
   accesability: 'slides/accesability/accesability.md',
   animations: './slides/animations/animations.md',
   boxModel: 'slides/box-model/box-model.md',
@@ -14,9 +14,16 @@ const lessons = {
   forms: 'slides/forms/forms.md',
   html: 'slides/intro-html/html.md',
   htmlCss: 'slides/html-css/html-css.md',
+  jquery: 'slides/intro-jquery/slides.md',
+  jqueryPlugins: 'slides/intro-jquery-plugins/slides.md',
+  js: 'slides/intro-js/slides.md',
   positioning: 'slides/positioning/positioning.md',
   responsive: 'slides/responsive/responsive.md',
   svg: 'slides/svg/svg.md',
+};
+
+const introToCode = {
+  introToCode: './slides/intro-to-code/intro-to-code.md',
 };
 
 const slideSettings = {
@@ -38,23 +45,32 @@ const showSlides = async lesson => {
   remark.create(slideSettings);
 };
 
-showSlides(lessons.animations);
+showSlides(introToCode.introToCode);
 
 /* 
-FEWD lessons
+fewdLessons
 
 1 html
 2 htmlCss
 3 boxModel
 4 cssSelectors
 5 flexbox
-6 
-8 positioning
-9 responsive
-10 forms
-11 animations
-12 svg
-13 accesability
+6 positioning
+7 responsive
+8
+9 js
+10 jquery
+11 jquery-plugins
+12 forms
+13 animations
+14 svg 
+15 accesability
+16 git / sass 
+*/
 
+/* 
+introToCode
+
+1 introToCode
 
 */
